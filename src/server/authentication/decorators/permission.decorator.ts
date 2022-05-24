cfce979122ab234799f59e1dee3f2e1e542bc111
yehezkieldio <yehezkieldio35@gmail.com>
2022-05-24 08:35:14 +0800

    chore(license): move to lgpl3.0

--- src/server/authentication/decorators/permission.decorator.ts
+++ src/server/authentication/decorators/permission.decorator.ts
@@ -1,3 +1,21 @@
+/**
+ * administrare - web platform for internal data management
+ * Copyright (C) 2022 imperatoria
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import { SetMetadata } from "@nestjs/common";
 import { PermissionLevel } from "@shared/typings/enumerations/permission-level.enum";
 
