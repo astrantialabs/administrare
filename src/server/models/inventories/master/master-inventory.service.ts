68d34fa818f7257d665467dcbbd5383436993214
yehezkieldio <yehezkieldio35@gmail.com>
2022-06-14 14:28:35 +0800

    feat: delete kategori and barang

--- src/server/models/inventories/master/master-inventory.service.ts
+++ src/server/models/inventories/master/master-inventory.service.ts
@@ -633,7 +633,6 @@ export class MasterInventoryService {
                     isKategori: true,
                     isWhiteSpace: false,
                 },
-
                 id: await romanizeNumber(category_index + 1),
                 kategori: category_object.kategori,
                 nama: category_object.kategori,
