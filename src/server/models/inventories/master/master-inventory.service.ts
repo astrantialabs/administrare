eb7e8ccb7703fe5f8c8c7b63a7515f65090bbb06
yehezkieldio <yehezkieldio35@gmail.com>
2022-06-14 11:19:10 +0800

    feat: table

--- src/server/models/inventories/master/master-inventory.service.ts
+++ src/server/models/inventories/master/master-inventory.service.ts
@@ -633,6 +633,7 @@ export class MasterInventoryService {
                     isKategori: true,
                     isWhiteSpace: false,
                 },
+
                 id: await romanizeNumber(category_index + 1),
                 kategori: category_object.kategori,
                 nama: category_object.kategori,
