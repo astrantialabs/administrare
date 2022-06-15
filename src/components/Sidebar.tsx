c5ae808d06a88749fd0f4d5f888d9f0ff757c987
yehezkieldio <yehezkieldio35@gmail.com>
2022-06-15 11:14:26 +0800

    fix: inventory manage actions button z index

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -167,14 +167,14 @@ export default function Sidebar({ type, children }: { type: string; children: Re
     const { isOpen, onOpen, onClose } = useDisclosure();
     return (
         <Box minH={`100vh`}>
-            <SidebarContent onClose={() => onClose} display={{ base: "none", md: "block" }} />
+            <SidebarContent onClose={() => onClose} display={{ base: "none", md: "block" }} zIndex={2} />
             <Drawer autoFocus={false} isOpen={isOpen} placement="left" onClose={onClose} returnFocusOnClose={false} onOverlayClick={onClose} size="full">
                 <DrawerContent>
                     <SidebarContent onClose={onClose} />
                 </DrawerContent>
             </Drawer>
             <MobileNavigation type={type} onOpen={onOpen} />
-            <Box ml={{ base: 0, md: 60 }} p={4} zIndex={`-1`}>
+            <Box ml={{ base: 0, md: 60 }} p={4} zIndex={3}>
                 {children}
             </Box>
         </Box>
