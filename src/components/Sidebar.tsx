203ce856b90fffbc0367797e0d8546d7ea4eb3f2
yehezkieldio <yehezkieldio35@gmail.com>
2022-06-19 10:36:43 +0800

    fix: #36

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -139,11 +139,11 @@ const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
                         Inventory
                     </NavigationItem>
 
-                    <NavigationItem link={`/inventory/demand`} icon={FiPieChart}>
-                        Inventory Demand
-                    </NavigationItem>
                     <NavigationItem link={`/inventory/request`} icon={FiPieChart}>
                         Inventory Request
+                    </NavigationItem>
+                    <NavigationItem link={`/inventory/demand`} icon={FiPieChart}>
+                        Inventory Demand
                     </NavigationItem>
                 </>
             )}
