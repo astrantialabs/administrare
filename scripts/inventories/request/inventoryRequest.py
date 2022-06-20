538ff513eaec940ccf4b607faf3b82c77e8e6ff0
NotHydra <rizky.irswanda115@gmail.com>
2022-06-21 00:24:41 +0800

    feat: raw request data group by username and date

--- scripts/inventories/request/inventoryRequest.py
+++ scripts/inventories/request/inventoryRequest.py
@@ -28,10 +28,12 @@ from utility import Utility
 
 class InventoryRequest():
     def main(currentDate):
+        
         filePath = f"../{Dependency.inventoryRequestFolderPath}/{currentDate}.xlsx"
 
         Excel.create_file(filePath)
         workbook = Excel(filePath)
+        workbook.change_sheet_name("Sheet", "Seluruh")
         workbook.set_zoom(85)
 
         requestCollection = Database.getCollection(Dependency.mongoDBURI, Dependency.databaseInventory, Dependency.collectionInventoryRequest)
@@ -40,23 +42,51 @@ class InventoryRequest():
         masterCollection = Database.getCollection(Dependency.mongoDBURI, Dependency.databaseInventory, Dependency.collectionInventoryMaster)
         inventoryMasterDocument = masterCollection.find_one({"tahun": 2022})
 
-        InventoryRequest.writeHeader(workbook)
-        InventoryRequest.writeMain(workbook, inventoryRequestDocument, inventoryMasterDocument)
+        userData = InventoryRequest.getUserData(inventoryRequestDocument)
+        Utility.writeJSON("./json/request.json", userData)  
+
+        InventoryRequest.writeHeaderNormal(workbook)
+        InventoryRequest.writeMainNormal(workbook, inventoryRequestDocument, inventoryMasterDocument)
+
+        for userObject in userData:
+            workbook.create_sheet(userObject.get("username"))
+            workbook.change_sheet(userObject.get("username"))
+            workbook.set_zoom(85)
+
+            requestData = { "barang": [] }
+
+            for dateObject in userObject.get("date"):
+                for requestObject in dateObject.get("request"):
+                    requestData.get("barang").append(requestObject)
+
+
+            InventoryRequest.writeHeaderNormal(workbook)
+            InventoryRequest.writeMainNormal(workbook, requestData, inventoryMasterDocument)
+
+            for dateObject in userObject.get("date"):
+                dateName = f'{userObject.get("username")} {"".join(dateObject.get("date").split("-"))}'
+                workbook.create_sheet(dateName)
+                workbook.change_sheet(dateName)
+                workbook.set_zoom(85)
+
+                InventoryRequest.writeHeaderNormal(workbook)
+                InventoryRequest.writeMainNormal(workbook, { "barang": dateObject.get("request") }, inventoryMasterDocument)
+                    
 
         workbook.save()
 
-    
-    def writeHeader(workbook):
+
+    def writeHeaderNormal(workbook):
         workbook.write_value_multiple("A1", "J1", ["No.", "Peminta", "Kategori", "Barang", "Jumlah", "Satuan", "Keterangan", "Dibuat", "Direspon", "Status"])
         workbook.font_multiple("A1", "J1", size = 12, bold = True)
         workbook.alignment_multiple("A1", "J1", vertical = "center", horizontal = "center")
         workbook.border_multiple("A1", "J1", "all", style = "thin")
 
     
-    def writeMain(workbook, inventoryRequestDocument, inventoryMasterDocument):
+    def writeMainNormal(workbook, requestData, masterData):
         rowCount = 2
-        for requestItemIndex, requestItemObject in enumerate(inventoryRequestDocument.get("barang")):
-            for masterCategoryObject in inventoryMasterDocument.get("kategori"):
+        for requestItemIndex, requestItemObject in enumerate(requestData.get("barang")):
+            for masterCategoryObject in masterData.get("kategori"):
                 if(requestItemObject.get("kategori_id") == masterCategoryObject.get("id")):
                     for masterItemObject in masterCategoryObject.get("barang"):
                         if(requestItemObject.get("barang_id") == masterItemObject.get("id")):
@@ -85,4 +115,55 @@ class InventoryRequest():
                             rowCount += 1
 
 
-        workbook.adjust_width("A1", ["J", rowCount], extra_width=1)
+        workbook.adjust_width("A1", ["J", rowCount], extra_width=2)
+
+
+    def getUserData(inventoryRequestDocument):
+        usernameArray = []
+        for requestMain in inventoryRequestDocument.get("barang"):
+            if(requestMain.get("username") not in usernameArray):
+                usernameArray.append(requestMain.get("username"))
+
+
+        userData = []
+        for usernameIndex, usernameItem in enumerate(usernameArray):
+            newUserObject = {
+                "id": usernameIndex + 1,
+                "username": usernameItem,
+                "date": []
+            }
+
+            userData.append(newUserObject)
+
+
+        for userObject in userData:
+            dateArray = []
+            for requestMain in inventoryRequestDocument.get("barang"):
+                createdAt = (requestMain.get("created_at").split(" "))[0]
+                
+                if(createdAt not in dateArray):
+                    dateArray.append(createdAt)
+
+
+            for dateIndex, dateItem in enumerate(dateArray):
+                newDateObject = {
+                    "id": dateIndex + 1,
+                    "date": dateItem,
+                    "request": []
+                }
+
+                userObject.get("date").append(newDateObject)
+
+        
+        for requestMain in inventoryRequestDocument.get("barang"):
+            for userObject in userData:
+                if(requestMain.get("username") == userObject.get("username")):
+                    for dateObject in userObject.get("date"):
+                        if((requestMain.get("created_at").split(" "))[0]) == dateObject.get("date"):
+                            newRequestObject = requestMain
+                            newRequestObject["internal_id"] = len(dateObject.get("request")) + 1
+
+                            dateObject.get("request").append(newRequestObject)
+
+
+        return userData
--- scripts/test.py
+++ scripts/test.py
@@ -1,3 +1,26 @@
+"""
+ # administrare - web platform for internal data management
+ # Copyright (C) 2022 astrantialabs
+ #
+ # This program is free software: you can redistribute it and/or modify
+ # it under the terms of the GNU General Public License as published by
+ # the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # You should have received a copy of the GNU General Public License
+ # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+"""
+ # @fileoverview The Test file.
+ # @author Rizky Irswanda <rizky.irswanda115@gmail.com>
+"""
+
 from utility import Utility
 
 from inventories.master.inventoryMaster import InventoryMaster
@@ -6,9 +29,9 @@ from inventories.demand.inventoryDemand import InventoryDemand
 
 class Test:
     def main():
-        InventoryMaster.main(Utility.currentDate())
+        # InventoryMaster.main(Utility.currentDate())
         InventoryRequest.main(Utility.currentDate())
-        InventoryDemand.main(Utility.currentDate())
+        # InventoryDemand.main(Utility.currentDate())
 
 
 Test.main() # Make sure you are on /scripts directory
