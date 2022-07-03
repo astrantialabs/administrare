981287b13dd24616e4ab99ea95e7d9b7a142697e
yehezkieldio <yehezkieldio35@gmail.com>
2022-07-03 11:41:42 +0800

    fix: remove top border from footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     const currentDate = new Date();
     return (
         <Box>
-            <Flex minHeight="80px" py={2} px={2} align="center" borderTop={1} borderStyle="solid" borderColor="#3B2C40">
+            <Flex minHeight="80px" py={2} px={2} align="center">
                 <Container maxWidth="1130px">
                     <Flex justify={{ base: "center", md: "center" }}>
                         <Text textAlign={{ base: "center", md: "center" }} fontFamily="heading" fontSize={{ base: "sm", md: "md" }}>
